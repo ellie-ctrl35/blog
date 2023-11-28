441c48e5a91d11977e87cf963bb4032508aa2bad
ellie-ctrl35 <146957037+ellie-ctrl35@users.noreply.github.com>
2023-11-28 08:43:12 +0000

    query all the post from backend

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,16 +1,31 @@
-import React,{useEffect} from 'react'
+import React,{useEffect,useState} from 'react'
 import axios from 'axios'
 
 const Home = () => {
+  const [posts,setPosts] = useState([])
+
 
    useEffect(() =>{
     axios.get('http://localhost:4001/getposts')
-    .then(post => console.log(posts))
+    .then(posts =>{
+      setPosts(posts.data)
+    })
     .catch(err => console.log(err))
    },[])
 
   return (
-    <div>Home</div>
+    <div style={{marginTop:"5%"}}>
+       <div style={{width:"100vw"}}>
+        {
+          posts.map(post =>(
+            <div style={{borderColor:"white",borderWidth:"2px",border:"solid",margin:"1%",width:'80%'}}>
+              <h1>{post.title}</h1>
+              <h2>{post.description}</h2>
+            </div>
+          ))
+        }
+       </div>
+    </div>
   )
 }
 
