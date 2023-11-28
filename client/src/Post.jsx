43a49590cf4bf364a98836b7c8396e10df8f84af
ellie-ctrl35 <146957037+ellie-ctrl35@users.noreply.github.com>
2023-11-28 15:11:36 +0000

    post

--- client/src/Post.jsx
+++ client/src/Post.jsx
@@ -1,14 +1,23 @@
-import React, { useEffect } from 'react'
+import axios from 'axios'
+import React, { useEffect ,useState} from 'react'
 import { useParams } from 'react-router-dom'
 
 const Post = () => {
     const {id}= useParams
+    const [post,setPost]= useState({})
     useEffect(() => {
-
+   axios.get('http://localhost:4001/getpostbyid')
+   .then(results =>setPost(results.data))
+   .catch(err => console.log(err))
     },[])
   return (
-    <div>Post</div>
+    <div>
+        <div>
+            <h1>{post.title}</h1>
+            <h2>{post.description}</h2>
+        </div>
+    </div>
   )
 }
 
-export default Post
+export default Post  
